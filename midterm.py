1d072052388fe755ed70d0639b93884f8d7ca601
Chinmai Raman <raman105@mail.chapman.edu>
2016-03-22 20:46:12 +0000

    first commit

---
+++ midterm.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+"""
+File: midterm.py
+Copyright (c) 2016 Chinmai Raman
+License: MIT
+Course: PHYS227
+Assignment: Midterm
+Date: March 22, 2016
+Email: raman105@mail.chapman.edu
+Name: Chinmai Raman
+Description: Midterm
+"""
+
+from __future__ import division
+import numpy as np
+import matplotlib.pyplot as plt
+
